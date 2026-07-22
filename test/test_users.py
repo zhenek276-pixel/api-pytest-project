3c65acffd257c60cd564f393ec6e8e194a4c6ac1
zhenek276-pixel <zhenek276@gmail.com>
2026-07-22 15:03:07 +0300

    Add Qase ids for user tests

--- test/test_users.py
+++ test/test_users.py
@@ -1,25 +1,29 @@
 import pytest
 
-from qase.pytest import qase
 from api_client import get_user, get_all_users
 
 
 
+@pytest.mark.qase_id(14)
 def test_user_name(user_data):
     assert user_data["username"] == "Bret"
 
 
+@pytest.mark.qase_id(15)
 def test_user_email_from_fixture(user_data):
     assert user_data["email"] == "Sincere@april.biz"
 
+@pytest.mark.qase_id(16)
 def test_user_id(user_data):
     assert user_data["id"] == 1
 
+@pytest.mark.qase_id(17)
 def test_users_404():
     response = get_user(9999)
 
     assert response.status_code == 404
 
+@pytest.mark.qase_id(18)
 @pytest.mark.parametrize(
     "user_id, expected_username",
     [
@@ -39,6 +43,7 @@ def test_user_username(user_id, expected_username):
     assert response.status_code == 200
     assert data["username"] == expected_username
 
+@pytest.mark.qase_id(19)
 @pytest.mark.parametrize(
     "user_id, expected_email",
     [
@@ -47,7 +52,6 @@ def test_user_username(user_id, expected_username):
         (3, "Nathan@yesenia.net")
     ]
 )
-
 def test_user_email_parametrize(user_id, expected_email):
     response = get_user(user_id)
     data = response.json()
@@ -55,18 +59,20 @@ def test_user_email_parametrize(user_id, expected_email):
     assert response.status_code == 200
     assert data["email"] == expected_email
 
-@qase.id(1)
+@pytest.mark.qase_id(20)
 def test_get_all_users_status_code():
     response = get_all_users()
     assert response.status_code == 200
 
-def test_get_all_users_not_empty():
+@pytest.mark.qase_id(21)
+def test_get_all_users_not_empty(): 
     response = get_all_users()
     data = response.json()
 
     assert response.status_code == 200
     assert len(data) > 0
 
+@pytest.mark.qase_id(22)
 def test_get_all_users_first_item_has_keys():
     response = get_all_users()
     data = response.json()
@@ -81,6 +87,7 @@ def test_get_all_users_first_item_has_keys():
     assert "email" in first_user
     assert "address" in first_user
 
+@pytest.mark.qase_id(23)
 def test_get_all_users_all_items_have_keys():
     response = get_all_users()
     data = response.json()
@@ -92,6 +99,7 @@ def test_get_all_users_all_items_have_keys():
         assert "email" in user
         assert "address" in user
 
+@pytest.mark.qase_id(24)
 def test_get_all_users_address_has_keys():
     response = get_all_users()
     data = response.json()
@@ -104,6 +112,7 @@ def test_get_all_users_address_has_keys():
         assert "zipcode" in address
         assert "geo" in address         
 
+@pytest.mark.qase_id(25)
 def test_get_all_users_geo_has_keys():
     response = get_all_users()
     data = response.json()
@@ -117,6 +126,7 @@ def test_get_all_users_geo_has_keys():
         assert "lat" in geo
         assert "lng" in geo
 
+@pytest.mark.qase_id(26)
 def test_get_all_users_email_contains_at():
     response = get_all_users()
     data = response.json()
@@ -124,7 +134,8 @@ def test_get_all_users_email_contains_at():
     for user in data:
         email = user["email"]
         assert "@" in email
-    
+
+@pytest.mark.qase_id(27)    
 def test_get_all_users_username_not_empty():
     response = get_all_users()
     data = response.json()
@@ -133,6 +144,7 @@ def test_get_all_users_username_not_empty():
         username = user["username"]
         assert len(username) > 0
 
+@pytest.mark.qase_id(28)
 def test_get_all_users_id_is_integer():
     response = get_all_users()
     data = response.json()
@@ -142,7 +154,8 @@ def test_get_all_users_id_is_integer():
     for user in data:
         user_id = user["id"]
         assert isinstance(user_id, int)
-        
+
+@pytest.mark.qase_id(29)        
 def test_get_all_users_company_has_keys():
     response = get_all_users()
     data = response.json()
@@ -156,6 +169,7 @@ def test_get_all_users_company_has_keys():
         assert "catchPhrase" in company
         assert "bs" in company
 
+@pytest.mark.qase_id(30)
 def test_get_all_users_company_name_not_empty():
     response = get_all_users()
     data = response.json()
